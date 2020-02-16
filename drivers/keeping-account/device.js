e32b31325027ee44360eb668adcb943711cf8c66
Thijs <thijs@305.nl>
2020-02-16 17:27:35 +0100

    Implemented Timesheet Toggle

--- drivers/keeping-account/device.js
+++ drivers/keeping-account/device.js
@@ -5,8 +5,12 @@ const { OAuth2Device } = require('homey-oauth2app');
 
 module.exports = class KeepingAccountDevice extends OAuth2Device {
 	
-	onOAuth2Init() {
+	async onOAuth2Init() {
 		this.log('KeepingAccountDevice has been inited');
+
+		this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+
+		await this.checkOngoingEntry()
 	}
 
 	onOAuth2Migrate() {
@@ -18,6 +22,63 @@ module.exports = class KeepingAccountDevice extends OAuth2Device {
 			sessionId,
 			configId
 		}  	
+	}
+
+	checkOngoingEntry()
+	{
+		const organisationId = this.getData().organisation.id;
+		const userId = this.getData().user.id;
+
+		this.setCapabilityValue('onoff', true)
+					.catch(this.error);
+
+		return this.oAuth2Client.getOngoingEntry(organisationId, userId)
+			.then(timeEntry => {
+
+				this.setCapabilityValue('onoff', true)
+					.catch(this.error);
+
+				return true;
+				
+			}).catch(error => {
+
+				this.setCapabilityValue('onoff', false)
+					.catch(this.error);
+
+				return false;
+			});
+	}
+
+	async onCapabilityOnoff( value, opts ) {
+
+		const organisationId = this.getData().organisation.id;
+		const userId = this.getData().user.id;
+
+		const on = await this.checkOngoingEntry();
+		
+		if (on) {
+			this.log('stopping entry');
+
+			const ongoing = await this.oAuth2Client.getOngoingEntry(organisationId, userId);
+
+			return this.oAuth2Client.stopEntry(organisationId, ongoing.id)
+				.then(timeEntry => {
+					this.setCapabilityValue('onoff', false)
+						.catch(this.error);
+					return true;
+				});
+		} else {
+			this.log('resuming entry');
+
+			const last = await this.oAuth2Client.getLastEntry(organisationId, userId);
+
+			return this.oAuth2Client.resumeEntry(organisationId, last.id)
+				.then(timeEntry => {
+					this.setCapabilityValue('onoff', true)
+						.catch(this.error);
+					return true;
+				});
+		}
 	}
 	
 };
